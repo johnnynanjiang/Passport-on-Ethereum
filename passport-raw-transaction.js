3c258030fe8ea2a3e5084642e0b7080b99b3ba38
Johnny Jiang <johnny.nan.jiang@gmail.com>
2024-06-15 11:35:49 +0800

    - add raw transaction in Uint8Array

--- passport-raw-transaction.js
+++ passport-raw-transaction.js
@@ -1,7 +1,5 @@
 const { Web3 } = require("web3");
-const { Chain, Common, Hardfork } = require('@ethereumjs/common');
-const { LegacyTransaction } = require('@ethereumjs/tx');
-const { bytesToHex } = require('@ethereumjs/util');
+const { encode } = require("rlp");
 
 async function main() {
   const web3 = new Web3("http://127.0.0.1:8545/");
@@ -9,25 +7,37 @@ async function main() {
   const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
   // import the Hardhat test account without the use of a wallet
   const sender = web3.eth.accounts.privateKeyToAccount(privateKey);
+
   const receiver = web3.eth.accounts.create();
 
-  const txParams = {
-    nonce: '0x00',
-    gasPrice: '0x09184e72a000',
-    gasLimit: '0x2710',
-    to: '0x0000000000000000000000000000000000000000',
-    value: '0x00',
-    data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
-  }
+  // used to calculate the transaction's maxFeePerGas
+  const block = await web3.eth.getBlock();
 
-  const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.Istanbul })
-  const tx = LegacyTransaction.fromTxData(txParams, { common })
-  console.log("tx", tx)
-  console.log("tx", tx.serialize())
+  const transaction = {
+    from: sender.address,
+    to: receiver.address,
+    value: 10,
+    // the following two properties must be included in raw transactions
+    maxFeePerGas: block.baseFeePerGas * 2n,
+    maxPriorityFeePerGas: 100000,
+  };
+  console.log("transaction", transaction);
 
-  const signedTx = tx.sign(Buffer.from(privateKey.substring(2, privateKey.length), 'hex'))
-  console.log("signedTx", signedTx)
-  console.log("signedTx", signedTx.serialize())
+  const signedTransaction = await web3.eth.accounts.signTransaction(
+    transaction,
+    sender.privateKey
+  );
+  console.log("signedTransaction", signedTransaction);
+
+  const receipt = await web3.eth.sendSignedTransaction(
+    signedTransaction.rawTransaction
+  );
+  console.log("receipt", receipt);
+
+  // Passport
+  const rawTransaction = encode([transaction.nonce, transaction.gasPrice, transaction.gasLimit, transaction.to, transaction.value, transaction.data]);
+  console.log("rawTransaction", new Uint8Array(rawTransaction));
+
 }
 
 main();
